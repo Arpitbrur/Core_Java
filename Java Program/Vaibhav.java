23c9b6bb86289a169057642bef620450701e4a0e
Arpit Brur <126546064+Arpitbrur@users.noreply.github.com>
2023-03-27 11:05:20 +0530

    Add files via upload

---
+++ Java Program/Vaibhav.java
@@ -0,0 +1,13 @@
+package java121;
+import java .util.Scanner;
+public class Vaibhav {
+	public static void main(String[]args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number");
+		String st= s.next();
+ char a[]=st.toCharArray();
+ System.out.println("digits in numberis :=");
+  System.out.println(a.length);
+	}
+
+}
